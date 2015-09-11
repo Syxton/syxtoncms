3af1a842279ea8e9782d7f2fa68479fc07beca8d
Syxton <davidso1@rose-hulman.edu>
2015-09-11 08:38:54 -0400

    IPN update to use cURL

--- scripts/paypal/ipn.php
+++ scripts/paypal/ipn.php
@@ -2,58 +2,85 @@
 if(!isset($CFG)){ include('../../config.php'); }
 include_once($CFG->dirroot . '/lib/header.php');
 
-// read the post from PayPal system and add 'cmd'
-$req = 'cmd=_notify-validate';
-
-foreach ($_POST as $key => $value){
-    $value = urlencode(stripslashes($value));
-    $req .= "&$key=$value";
+// STEP 1: read POST data
+// Reading POSTed data directly from $_POST causes serialization issues with array data in the POST.
+// Instead, read raw POST data from the input stream. 
+$raw_post_data = file_get_contents('php://input');
+$raw_post_array = explode('&', $raw_post_data);
+$myPost = array();
+foreach ($raw_post_array as $keyval) {
+  $keyval = explode ('=', $keyval);
+  if (count($keyval) == 2)
+     $myPost[$keyval[0]] = urldecode($keyval[1]);
 }
+// read the IPN message sent from PayPal and prepend 'cmd=_notify-validate'
+$req = 'cmd=_notify-validate';
+if(function_exists('get_magic_quotes_gpc')) {
+   $get_magic_quotes_exists = true;
+} 
+foreach ($myPost as $key => $value) {        
+   if($get_magic_quotes_exists == true && get_magic_quotes_gpc() == 1) { 
+        $value = urlencode(stripslashes($value)); 
+   } else {
+        $value = urlencode($value);
+   }
+   $req .= "&$key=$value";
+}
+ 
+// STEP 2: POST IPN data back to PayPal to validate
+if($CFG->paypal){ 
+        $ch = curl_init('https://www.paypal.com/cgi-bin/webscr');
+}else{ 
+        $ch = curl_init('https://www.sandbox.paypal.com/cgi-bin/websc
+    '); }
 
-// post back to PayPal system to validate
-$header = "POST /cgi-bin/webscr HTTP/1.0\r\n";
-$header .= "Content-Type: application/x-www-form-urlencoded\r\n";
-$header .= "Content-Length: " . strlen($req) . "\r\n\r\n";
+curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Connection: Close'));
+// In wamp-like environments that do not come bundled with root authority certificates,
+// please download 'cacert.pem' from "http://curl.haxx.se/docs/caextract.html" and set 
+// the directory path of the certificate as shown below:
+// curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . '/cacert.pem');
+if( !($res = curl_exec($ch)) ) {
+    // error_log("Got " . curl_error($ch) . " when processing IPN data");
+    curl_close($ch);
+    exit;
+}
+curl_close($ch);
+ 
+// STEP 3: Inspect IPN validation result and act accordingly
+if (strcmp ($res, "VERIFIED") == 0) {
+    // The IPN is verified, process it:
+    // check whether the payment_status is Completed
+    // check that txn_id has not been previously processed
+    // check that receiver_email is your Primary PayPal email
+    // check that payment_amount/payment_currency are correct
+    // process the notification
+    // assign posted variables to local variables
+        
+	if(!get_db_row("SELECT * FROM logfile WHERE feature='events' AND description='Paypal' AND info='".$_POST['txn_id']."'")){
+		$regids = $_POST['custom'];
+		$regids = explode(":",$regids);
+		$i=0;
+		while(isset($regids[$i])){
+			$paid = get_db_field("value", "events_registrations_values", "elementname='paid' AND regid=".$regids[$i]);
+			$SQL = "UPDATE events_registrations_values SET value=".($paid + $_POST["mc_gross_".($i+1)])." WHERE elementname='paid' AND regid=".$regids[$i];
+			execute_db_sql($SQL);
+			$i++;
+		}
 
-if($CFG->paypal){ $fp = fsockopen ('www.paypal.com', 80, $errno, $errstr, 30);
-}else{ $fp = fsockopen ('www.sandbox.paypal.com', 80, $errno, $errstr, 30); }
-
-//$fp = fsockopen ('ssl://www.paypal.com', 443, $errno, $errstr, 30);
-
-if(!$fp){
-// HTTP ERROR
-}else{
-	fputs($fp, $header . $req);
-		while(!feof($fp)){
-		$res = fgets ($fp, 1024);
-		if (strcmp ($res, "VERIFIED") == 0) {
-			// check the payment_status is Completed
-			// check that txn_id has not been previously processed
-			// check that receiver_email is your Primary PayPal email
-			// check that payment_amount/payment_currency are correct
-			// process payment
-			
-			$keyarray = $_POST;
-			if(!get_db_row("SELECT * FROM logfile WHERE feature='events' AND description='Paypal' AND info='".$keyarray['txn_id']."'")){
-				$regids = $keyarray['custom'];
-				$regids = explode(":",$regids);
-				$i=0;
-				while(isset($regids[$i])){
-					$paid = get_db_field("value", "events_registrations_values", "elementname='paid' AND regid=".$regids[$i]);
-					$SQL = "UPDATE events_registrations_values SET value=".($paid + $keyarray["mc_gross_".($i+1)])." WHERE elementname='paid' AND regid=".$regids[$i];
-					execute_db_sql($SQL);
-					$i++;
-				}
-
-				//Log
-				log_entry('events', $keyarray['txn_id'], "Paypal");
-			}
-		}elseif (strcmp ($res, "INVALID") == 0){
-    		//Log
-    		log_entry('events', $res, "Paypal (failed)");
-	  	}
+		//Log
+		log_entry('events', $_POST['txn_id'], "Paypal");
 	}
-fclose ($fp);
+} else if (strcmp ($res, "INVALID") == 0) {
+    // IPN invalid, log for manual investigation
+    //Log
+    log_entry('events', $res, "Paypal (failed)");
 }
 ?>
 
