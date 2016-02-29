90ed4fb522b380992ca0c6e6cc11eb763718f411
Matt Davidson <davidso1@rose-hulman.edu>
2016-02-29 14:04:30 -0500

    Template improvments

--- features/events/templates/simple_contact_form/backend.php
+++ features/events/templates/simple_contact_form/backend.php
@@ -83,7 +83,7 @@ error_reporting(E_ERROR | E_PARSE); //keep warnings from showing
                 $i++;           
             }
 
-            echo '<div style="margin:auto;width:90%;text-align:center;"><h3>You have successfully registered '.$MYVARS->GET["Name_First"].' for '.$event['name'] . '.</h3>';
+            echo '<div style="margin:auto;width:90%;text-align:center;"><h3>You have successfully registered '.$MYVARS->GET["Name_First"].' '.$MYVARS->GET["Name_Last"].'<br />for<br />'.$event['name'] . '.</h3>';
        
             execute_db_sql("UPDATE events_registrations SET verified='1' WHERE regid='$regid'");
             
@@ -100,18 +100,24 @@ error_reporting(E_ERROR | E_PARSE); //keep warnings from showing
     		}
             
 			if($MYVARS->GET["cart_total"] > 0){ // Event paid by paypal.
-                echo '<br />You have chosen to pay:  <span style="color:blue;font-size:1.25em;">$'.number_format($MYVARS->GET["cart_total"],2).'</span> right now, with full payment expected when you show up to the event.';
-                echo '<br />You must pay in full to attend.  The price of your registration is <span style="color:blue;font-size:1.25em;">$'.number_format($event['fee_full'],2).'</span>';
-                echo '<br />Click the Paypal button below to make a payment.
-  				    <br /><br />
+                
+                echo '<br />The full payment amount of <span style="color:blue;font-size:1.25em;">$'.number_format($event['fee_full'],2).'</span> will be expected when you show up to the event.'; 
+                if($MYVARS->GET["cart_total"] < $MYVARS->GET["total_owed"]) {
+                    echo '<br />You have indicated that you would like to pay:  <span style="color:blue;font-size:1.25em;">$'.number_format($MYVARS->GET["cart_total"],2).'</span> right now.';    
+                } else {
+                    echo '<br />You have indicated that you would like to pay the full event cost of:  <span style="color:blue;font-size:1.25em;">$'.number_format($MYVARS->GET["cart_total"],2).'</span> right now.';
+                }
+                
+                echo '<br /><br />Click the Paypal button below to make that payment.
+  				    <br />
                     <div style="text-align:center;">
     				'.make_paypal_button($cart_items,$event['paypal']).'
     				</div>';
             } else {
-                echo '<br />Please bring cash, check or money order in the amount of <span style="color:blue;font-size:1.25em;">$'.number_format($event['fee_full'],2).'</span> payable to <strong>'.$event["payableto"].'</strong>.';
+                echo '<br />Please bring cash, check or money order in the amount of <span style="color:blue;font-size:1.25em;">$'.number_format($event['fee_full'],2).'</span><br />payable to <strong>'.$event["payableto"].'</strong> on the day of the event.';
             }
 		} else { // Support for a free event
-            echo '<div style="margin:auto;width:90%;text-align:center;"><h3>You have successfully registered '.$MYVARS->GET["Name_First"].' for '.$event['name'] . '.</h3>';
+            echo '<div style="margin:auto;width:90%;text-align:center;"><h3>You have successfully registered '.$MYVARS->GET["Name_First"].' '.$MYVARS->GET["Name_Last"].'<br />for<br />'.$event['name'] . '.</h3>';
             execute_db_sql("UPDATE events_registrations SET verified='1' WHERE regid='$regid'");
                 
             //Send registration email
@@ -127,53 +133,14 @@ error_reporting(E_ERROR | E_PARSE); //keep warnings from showing
     		}
         }
 		
+        echo "<h2>Thank you for registering!</h2>";
+
         //Facebook share button
-        echo '<br />' . facebook_share_button($eventid,$MYVARS->GET["Name_First"],$keys);
-        
-        echo '</div>';
-	} else { // Failed registration
-		$MYVARS->GET["cart_total"] = $MYVARS->GET["cart_total"] - $MYVARS->GET["payment_amount"];
-        $MYVARS->GET["total_owed"] = $MYVARS->GET["total_owed"] - $MYVARS->GET["owed"];
-		echo '<div style="text-align:center;"><div style="width:60%;margin:auto;"><span class="error_text">Your registration for '.$event['name'].' has failed. </span><br /> '.$error . '</div>';	
-		if(isset($MYVARS->GET["items"])){ //other registrations have already occured
-			if(!empty($MYVARS->GET["cart_total"]) && !empty($MYVARS->GET["items"])) {
-                $items = $MYVARS->GET["items"];
-				echo '<div id="backup">
-                        <input type="hidden" name="cart_total" id="cart_total" value="'.$MYVARS->GET["cart_total"].'" />
-                        <input type="hidden" name="total_owed" id="total_owed" value="'.$MYVARS->GET["total_owed"].'" />
-                        <input type="hidden" name="items" id="items" value="'.$items.'" /></div>';
-				
-				$items = explode("**",$items);
-                $i=0;
-                foreach($items as $item){
-					$itm = explode("::",$item);
-					$cart_items[$i]->regid = $itm[0];
-					$cart_items[$i]->description = $itm[1];
-					$cart_items[$i]->cost = $itm[2];                    
-                    $i++;
-                }
-				
-				if($MYVARS->GET['payment_method'] == "PayPal"){
-					echo '<br />
-					If you would like to pay the <span style="color:blue;font-size:1.25em;">$'.$MYVARS->GET["cart_total"].'</span> fee now, click the Paypal button below.
-					<br /><br />
-                    <div style="text-align:center;">
-					   '.make_paypal_button($cart_items,$event['paypal']).'
-					</div>';	
-				}else{ //Pay by check
-					echo '<br />
-					If you are done with the registration process, please make out your <br />
-					check or money order in the amount of <span style="color:blue;font-size:1.25em;">$'.$MYVARS->GET["cart_total"].'</span> payable to <b>'.$event["payableto"].'</b> and send it to <br /><br />
-					<br />
-                    <div style="text-align:center;">
-					   '.$event['checksaddress'].'.  
-					</div>';
-				}
-                
-                echo "<br /><br /><strong>Thank you for registering for this event.</strong>";	                   
-			}
-		}
-        echo "</div>";
+        echo '<br /><br />' . facebook_share_button($eventid,$MYVARS->GET["Name_First"],$keys);
+
+	} else { // Failed registration      
+        echo "<br /><br /><strong>We were unable to register you for this event.  Please try again at a later date.</strong>";
 	}
+    echo "</div>";
 }
 ?>
