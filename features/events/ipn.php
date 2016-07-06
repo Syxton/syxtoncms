844073305df459ba9d9e5c9362d7fa04539d4d4e
Matt Davidson <davidso1@rose-hulman.edu>
2016-07-06 14:27:46 -0400

    IPN fixes to events area.

--- features/events/ipn.php
+++ features/events/ipn.php
@@ -69,24 +69,36 @@ if (strcmp ($res, "VERIFIED") == 0) {
     // check that receiver_email is your Primary PayPal email
     // check that payment_amount/payment_currency are correct
     // process payment
-    
-	$keyarray = $_POST;
-	if(!get_db_row("SELECT * FROM logfile WHERE feature='events' AND description='Paypal' AND info='".$keyarray['txn_id']."'")){
-		$regids = $keyarray['custom'];
+
+    $txid = $_POST['txn_id'];
+	if(!get_db_row("SELECT * FROM logfile WHERE feature='events' AND description='Paypal' AND info='$txid'")){
+		$regids = $_POST['custom'];
 		$regids = explode(":",$regids);
 		$i=0;
 		while(isset($regids[$i])){
-			$paid = get_db_field("value", "events_registrations_values", "elementname='paid' AND regid=".$regids[$i]);
-			$SQL = "UPDATE events_registrations_values SET value='".((float) $paid + (float) $keyarray["mc_gross_".($i+1)])."' WHERE elementname='paid' AND regid='".$regids[$i]."'";
+            $regid = dbescape($regids[$i]);
+            $add = $_POST["mc_gross_".($i+1)];
+
+			$paid = get_db_field("value", "events_registrations_values", "elementname='paid' AND regid='$regid'");
+			$SQL = "UPDATE events_registrations_values SET value='".((float) $paid + (float) $add)."' WHERE elementname='paid' AND regid='$regid'";
 			execute_db_sql($SQL);
             
             //If payment is made, it is no longer in queue
-            $SQL = "UPDATE events_registrations SET verified='1' WHERE regid='".$regids[$i]."'";
+            $SQL = "UPDATE events_registrations SET verified='1' WHERE regid='$regid'";
 			execute_db_sql($SQL);
+            
+            // Make an entry for this transaction that links it to this registration.
+            $eventid = get_db_field("eventid","events_registrations_values","regid='$regid'"); // Get eventid
+            $params = array("date" => get_timestamp(),
+                            "amount" => $add,
+                            "txid" => $txid);
+            $SQL = "INSERT INTO events_registrations_values (eventid, elementname, regid, value) VALUES('$eventid','tx','$regid','".dbescape(serialize($params))."')";
+            execute_db_sql($SQL);
+
             $touser = new stdClass();
-            $touser->fname = get_db_field("value", "events_registrations_values", "regid='".$regids[$i]."' AND elementname='Camper_Name_First'");
-    		$touser->lname = get_db_field("value", "events_registrations_values", "regid='".$regids[$i]."' AND elementname='Camper_Name_Last'");
-    		$touser->email = get_db_field("email","events_registrations","regid='".$regids[$i]."'");
+            $touser->fname = get_db_field("value", "events_registrations_values", "regid='$regid' AND elementname='Camper_Name_First'");
+    		$touser->lname = get_db_field("value", "events_registrations_values", "regid='$regid' AND elementname='Camper_Name_Last'");
+    		$touser->email = get_db_field("email","events_registrations","regid='$regid'");
     		
 		$fromuser = new stdClass();
 		$fromuser->email = $CFG->siteemail;
@@ -100,7 +112,7 @@ if (strcmp ($res, "VERIFIED") == 0) {
 		}
 
 		//Log
-		log_entry('events', $keyarray['txn_id'], "Paypal");
+		log_entry('events', $txid, "Paypal");
 	}
     
 } else if (strcmp ($res, "INVALID") == 0) {
@@ -110,64 +122,4 @@ if (strcmp ($res, "VERIFIED") == 0) {
     //Log
     log_entry('events', $res, "Paypal (none)");
 }
-//
-//
-//
-//
-//if(!isset($CFG)){ include('../../config.php'); }
-//include_once($CFG->dirroot . '/lib/header.php');
-//
-//// read the post from PayPal system and add 'cmd'
-//$req = 'cmd=_notify-validate';
-//
-//foreach ($_POST as $key => $value){
-//    $value = urlencode(stripslashes($value));
-//    $req .= "&$key=$value";
-//}
-//
-//// post back to PayPal system to validate
-//$header = "POST /cgi-bin/webscr HTTP/1.0\r\n";
-//$header .= "Content-Type: application/x-www-form-urlencoded\r\n";
-//$header .= "Content-Length: " . strlen($req) . "\r\n\r\n";
-//
-//if($CFG->paypal){ $fp = fsockopen ('www.paypal.com', 80, $errno, $errstr, 30);
-//}else{ $fp = fsockopen ('www.sandbox.paypal.com', 80, $errno, $errstr, 30); }
-//
-////$fp = fsockopen ('ssl://www.paypal.com', 443, $errno, $errstr, 30);
-//
-//if(!$fp){
-//// HTTP ERROR
-//}else{
-//	fputs($fp, $header . $req);
-//		while(!feof($fp)){
-//		$res = fgets ($fp, 1024);
-//		if (strcmp ($res, "VERIFIED") == 0) {
-//			// check the payment_status is Completed
-//			// check that txn_id has not been previously processed
-//			// check that receiver_email is your Primary PayPal email
-//			// check that payment_amount/payment_currency are correct
-//			// process payment
-//			
-//			$keyarray = $_POST;
-//			if(!get_db_row("SELECT * FROM logfile WHERE feature='events' AND description='Paypal' AND info='".$keyarray['txn_id']."'")){
-//				$regids = $keyarray['custom'];
-//				$regids = explode(":",$regids);
-//				$i=0;
-//				while(isset($regids[$i])){
-//					$paid = get_db_field("value", "events_registrations_values", "elementname='paid' AND regid=".$regids[$i]);
-//					$SQL = "UPDATE events_registrations_values SET value=".($paid + $keyarray["mc_gross_".($i+1)])." WHERE elementname='paid' AND regid=".$regids[$i];
-//					execute_db_sql($SQL);
-//					$i++;
-//				}
-//
-//				//Log
-//				log_entry('events', $keyarray['txn_id'], "Paypal");
-//			}
-//		}elseif (strcmp ($res, "INVALID") == 0){
-//    		//Log
-//    		log_entry('events', $res, "Paypal (failed)");
-//	  	}
-//	}
-//fclose ($fp);
-//}
 ?>
