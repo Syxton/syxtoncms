3e151142c49dff28bdd250adf04e083a5a2ab82f
Matt Davidson <davidso1@rose-hulman.edu>
2021-09-30 08:45:10 -0400

    clean up ipn and add a few checks

--- features/events/ipn.php
+++ features/events/ipn.php
@@ -47,12 +47,9 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array('Connection: Close'));
 
 curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . '/cacert.pem');
 if ( !($res = curl_exec($ch)) ) {
-    // error_log("Got " . curl_error($ch) . " when processing IPN data");
-    //Log
-    log_entry('events', "Got " . curl_error($ch) . " when processing IPN data", "Paypal (failed)");
-
-    curl_close($ch);
-    exit;
+  log_entry('events', "Got " . curl_error($ch) . " when processing IPN data", "Paypal (failed)"); // Log
+  curl_close($ch);
+  exit;
 }
 curl_close($ch);
 
@@ -60,64 +57,59 @@ curl_close($ch);
 $req = str_replace("&", "||", $req);  // Make it a nice list in case we want to email it to ourselves for reporting
 
 if (strcmp ($res, "VERIFIED") == 0) {
-
-    // check whether the payment_status is Completed
-    // check that txn_id has not been previously processed
-    // check that receiver_email is your Primary PayPal email
-    // check that payment_amount/payment_currency are correct
-    // process payment
+  // check whether the payment_status is Completed
+  // check that txn_id has not been previously processed
+  // check that receiver_email is your Primary PayPal email
+  // check that payment_amount/payment_currency are correct
+  // process payment
 
 	$keyarray = $_POST;
-    $txid = $_POST['txn_id'];
-	if (!get_db_row("SELECT * FROM logfile WHERE feature='events' AND description='Paypal' AND info='$txid'")) {
+  $txid = $_POST['txn_id'];
+	if (!empty($txid) && !get_db_row("SELECT * FROM logfile WHERE feature='events' AND description='Paypal' AND info='$txid'")) {
 		$regids = $keyarray['custom'];
 		$regids = explode(":",$regids);
 		$i=0;
 		while (isset($regids[$i])) {
-            $regid = dbescape($regids[$i]);
-            $add = $_POST["mc_gross_".($i+1)];
-
+      $regid = dbescape($regids[$i]);
+      $add = $_POST["mc_gross_".($i+1)];
 			$paid = get_db_field("value", "events_registrations_values", "elementname='paid' AND regid='$regid'");
 			$SQL = "UPDATE events_registrations_values SET value='".((float) $paid + (float) $add)."' WHERE elementname='paid' AND regid='$regid'";
 			execute_db_sql($SQL);
 
-            // If payment is made, it is no longer in queue.
-            $SQL = "UPDATE events_registrations SET verified='1' WHERE regid='$regid'";
+      // If payment is made, it is no longer in queue.
+      $SQL = "UPDATE events_registrations SET verified='1' WHERE regid='$regid'";
 			execute_db_sql($SQL);
 
-            // Make an entry for this transaction that links it to this registration.
-            $eventid = get_db_field("eventid","events_registrations_values","regid='$regid'"); // Get eventid.
-            $params = array("date" => get_timestamp(),
-                            "amount" => $add,
-                            "txid" => $txid);
-            $SQL = "INSERT INTO events_registrations_values (eventid, elementname, regid, value) VALUES('$eventid','tx','$regid','".dbescape(serialize($params))."')";
-            execute_db_sql($SQL);
+      // Make an entry for this transaction that links it to this registration.
+      $eventid = get_db_field("eventid","events_registrations_values","regid='$regid'"); // Get eventid.
+      $params = array("date" => get_timestamp(),
+                    "amount" => $add,
+                      "txid" => $txid);
+      $SQL = "INSERT INTO events_registrations_values (eventid, elementname, regid, value) VALUES('$eventid','tx','$regid','".dbescape(serialize($params))."')";
+      execute_db_sql($SQL);
 
-            $touser = new \stdClass;
-            $touser->fname = get_db_field("value", "events_registrations_values", "regid='$regid' AND elementname='Camper_Name_First'");
-    		$touser->lname = get_db_field("value", "events_registrations_values", "regid='$regid' AND elementname='Camper_Name_Last'");
-    		$touser->email = get_db_field("email","events_registrations","regid='$regid'");
+      $touser = new \stdClass;
+      $touser->fname = get_db_field("value", "events_registrations_values", "regid='$regid' AND elementname='Camper_Name_First'");
+  		$touser->lname = get_db_field("value", "events_registrations_values", "regid='$regid' AND elementname='Camper_Name_Last'");
+  		$touser->email = get_db_field("email","events_registrations","regid='$regid'");
 
-            $fromuser = new \stdClass;
-            $fromuser->email = $CFG->siteemail;
-    		$fromuser->fname = $CFG->sitename;
-    		$fromuser->lname = "";
-    		$message = registration_email($regids[$i], $touser);
-    		if (send_email($touser, $fromuser, null, $CFG->sitename . " Registration", $message)) {
-    			send_email($fromuser, $fromuser, null, $CFG->sitename . " Registration", $message);
-    		}
+      $fromuser = new \stdClass;
+      $fromuser->email = $CFG->siteemail;
+  		$fromuser->fname = $CFG->sitename;
+  		$fromuser->lname = "";
+
+      if (!empty($touser->email)) {
+        $message = registration_email($regids[$i], $touser);
+        send_email($fromuser, $fromuser, null, $CFG->sitename . " Registration", $message);
+    		send_email($touser, $fromuser, null, $CFG->sitename . " Registration", $message);
+      }
 			$i++;
 		}
-
-		//Log
-		log_entry('events', $keyarray['txn_id'], "Paypal");
+		log_entry('events', $keyarray['txn_id'], "Paypal"); // Log
 	}
-
 } else if (strcmp ($res, "INVALID") == 0) {
-    //Log
-    log_entry('events', $res, "Paypal (failed)");
+  log_entry('events', $res, "Paypal (failed)"); // Log
 } else {
-    //Log
-    log_entry('events', $res, "Paypal (none)");
+  log_entry('events', $res, "Paypal (none)"); // Log
 }
 ?>
