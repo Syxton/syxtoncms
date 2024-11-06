85c44b020eaea01fae28480a07f2558aa13c3df2
Matt Davidson <davidso1@rose-hulman.edu>
2024-11-06 09:13:12 -0500

    add comment to config sample

--- config-sample.php
+++ config-sample.php
@@ -1,71 +1,113 @@
 <?php  // SyxtonCMS Configuration File
 
+// Unset any existing configuration and user variables
 unset($CFG);
 unset($USER);
 
+// Initialize configuration as an empty object
 $CFG = new \stdClass;
 
-//Downtime
+/**
+ * Downtime Configuration
+ * @var bool $downtime Indicates if the site is in downtime
+ * @var string $alternatepage Path to the alternate page during downtime
+ * @var string $safeip Comma-separated list of safe IP addresses
+ */
 $CFG->downtime = false;
 $CFG->alternatepage = 'down.html';
-$CFG->safeip = ',192.168.0.1,192.186.0.2,'; //Must have a comma on both sides of ip address
+$CFG->safeip = ',192.168.0.1,192.186.0.2,'; // Must have a comma on both sides of IP address
 
-//Website info
-$CFG->sitename 	= 'Website Name';
-$CFG->siteowner	= 'Your Name';
-$CFG->siteemail	= 'test@email.com';
+/**
+ * Website Information
+ * @var string $sitename Name of the site
+ * @var string $siteowner Owner of the site
+ * @var string $siteemail Contact email of the site
+ * @var string $sitefooter Footer text of the site
+ * @var string $logofile Path to the site logo inside {userfilesfolder}/branding/logos/
+ * @var string $mobilelogofile Path to the mobile site logo inside {userfilesfolder}/branding/logos/
+ */
+$CFG->sitename = 'Website Name';
+$CFG->siteowner = 'Your Name';
+$CFG->siteemail = 'test@email.com';
 $CFG->sitefooter = '1234 My Address';
-$CFG->logofile 	= 'logo.png';
-$CFG->mobilelogofile 	= 'mobilelogo.png';
+$CFG->logofile = 'logo.png';
+$CFG->mobilelogofile = 'mobilelogo.png';
 
-//Database connection variables
-$CFG->dbtype    = 'mysqli'; //mysql or mysqli
-$CFG->dbhost    = 'localhost';
-$CFG->dbname    = 'mydbname';
-$CFG->dbuser    = 'mydbuser';
-$CFG->dbpass    = 'mydbpassword';
+/**
+ * Database Connection Variables
+ * @var string $dbtype Type of database (mysql or mysqli)
+ * @var string $dbhost Database host
+ * @var string $dbname Database name
+ * @var string $dbuser Database user
+ * @var string $dbpass Database password
+ */
+$CFG->dbtype = 'mysqli'; // mysql or mysqli
+$CFG->dbhost = 'localhost';
+$CFG->dbname = 'mydbname';
+$CFG->dbuser = 'mydbuser';
+$CFG->dbpass = 'mydbpassword';
 
-//SMTP server
-$CFG->smtppath	= '';
-$CFG->smtp	= false;
+/**
+ * SMTP Server Configuration
+ * @var string $smtppath Path to SMTP server
+ * @var bool $smtp Enable or disable SMTP
+ * @var bool $smtpauth Enable or disable SMTP authentication
+ * @var string $smtpuser SMTP username
+ * @var string $smtppass SMTP password
+ */
+$CFG->smtppath = '';
+$CFG->smtp = false;
 $CFG->smtpauth = true;
-$CFG->smtpuser	= '';
-$CFG->smtppass	= '';
+$CFG->smtpuser = '';
+$CFG->smtppass = '';
 
-//Directory variables
-$CFG->directory = 'mywebsites/syxtoncms';
-$CFG->wwwroot   = '//' . $_SERVER['SERVER_NAME'];
-$CFG->wwwroot   = $CFG->directory ? $CFG->wwwroot . '/' . $CFG->directory : $CFG->wwwroot;
-$CFG->docroot   = dirname(__FILE__);
-$CFG->dirroot   = $CFG->docroot;
+/**
+ * Directory Variables
+ * @var string $directory Directory path for the CMS
+ * @var string $wwwroot Web root URL
+ * @var string $docroot Document root directory
+ * @var string $dirroot Directory root
+ */
+$CFG->directory = 'mywebsites/syxtoncms'; // Points to http://localhost/mywebsites/syxtoncms
+$CFG->wwwroot = '//' . $_SERVER['SERVER_NAME'];
+$CFG->wwwroot = $CFG->directory ? $CFG->wwwroot . '/' . $CFG->directory : $CFG->wwwroot;
+$CFG->docroot = dirname(__FILE__);
+$CFG->dirroot = $CFG->docroot;
 
-//Userfile path
+/**
+ * Userfile Path Configuration
+ * @var string $userfilesfolder Folder for user files
+ * @var string $userfilespath Path to the user files folder
+ * @var string $userfilesurl URL to access user files
+ */
 $CFG->userfilesfolder = 'userfiles';
 $CFG->userfilespath = $CFG->docroot . '\\' . $CFG->userfilesfolder;
 $CFG->userfilesurl = $CFG->wwwroot . '/' . $CFG->userfilesfolder;
 
-//Home site id
+/**
+ * Miscellaneous Configuration
+ * @var int $SITEID Home site ID
+ * @var bool $paypal Enable or disable PayPal
+ * @var string $paypal_merchant_account PayPal merchant account email
+ * @var string $paypal_auth PayPal authentication token
+ * @var int $cookietimeout Cookie timeout in seconds
+ * @var string $timezone Default timezone
+ * @var string $defaultaddress Default address
+ * @var string $googleapikey Google Maps API site key
+ * @var string $geolocationkey Geolocation API key
+ * @var string $analytics Google Analytics ID
+ * @var int $debug Debug level (0 no errors, 1 log errors, 2 log and print, 3 log and print with extra debug)
+ */
 $CFG->SITEID = 1;
-
-//Paypal Live
 $CFG->paypal = true;
 $CFG->paypal_merchant_account = 'test@email.com';
 $CFG->paypal_auth = '';
-
-//Cookie variables in seconds
 $CFG->cookietimeout = 600;
 $CFG->timezone = "America/Indiana/Indianapolis";
 $CFG->defaultaddress = "My City, IL";
-
-//Google Maps API site key
 $CFG->googleapikey = "";
-
-//Geolocation API key (https://extreme-ip-lookup.com/)
 $CFG->geolocationkey = "";
-
-//Google Analytics id
 $CFG->analytics = '';
-
-// Debug level. 0 no errors, 1 log errors, 2 log and print, 3 log and print with extra debug.
 $CFG->debug = 0;
 ?>
+
