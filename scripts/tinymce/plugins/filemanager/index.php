93fc553588c6d679d5f203f1fa8cc1d80e0712ce
Matt Davidson <davidso1@rose-hulman.edu>
2026-09-22 09:06:52 -0400

    size optimization

--- scripts/tinymce/plugins/filemanager/index.php
+++ scripts/tinymce/plugins/filemanager/index.php
@@ -74,7 +74,7 @@ if (empty($_SESSION['fm_csrf'])) {
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
 <title>File Manager</title>
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style-min.css">
 </head>
 <body>
 <div id="fm-root"
